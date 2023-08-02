233691d617ad0dabc1a2c8d9dfa3c742d6d1d496
Osama <oilyascs@gmail.com>
2023-08-02 23:39:12 +0100

    added basic card with a table inside.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,37 +6,40 @@ export default function Index() {
           ShinyDex
         </h1>
         <div className="flex justify-center items-center">
-          <div className="max-w-md py-4 px-8 bg-sky-500 shadow-lg rounded-lg my-20">
+          <div className="max-w-md py-4 px-8 shadow-lg rounded-lg my-20">
             <div>
               <h2 className=" flex items-center justify-center text-gray-800 text-3xl font-semibold text-center ">
                 Design Tools
               </h2>
-              <table className="table-fixed">
+              <br/>
+              <div className="">
+              <table className="table-fixed border-separate border border-yellow-200">
                 <thead>
                   <tr>
-                    <th>Song</th>
-                    <th>Artist</th>
-                    <th>Year</th>
+                    <th className="border border-yellow-200 w-1/3">Song</th>
+                    <th className="border border-yellow-200 w-1/3">Artist</th>
+                    <th className="border border-yellow-200 w-1/3">Year</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    <td>Malcolm Lockyer</td>
-                    <td>1961</td>
+                    <td className="border border-yellow-200">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    <td className="border border-yellow-200">Malcolm Lockyer</td>
+                    <td className="border border-yellow-200">1961</td>
                   </tr>
                   <tr>
-                    <td>Witchy Woman</td>
-                    <td>The Eagles</td>
-                    <td>1972</td>
+                    <td className="border border-yellow-200">Witchy Woman</td>
+                    <td className="border border-yellow-200">The Eagles</td>
+                    <td className="border border-yellow-200">1972</td>
                   </tr>
                   <tr>
-                    <td>Shining Star</td>
-                    <td>Earth, Wind, and Fire</td>
-                    <td>1975</td>
+                    <td className="border border-yellow-200">Shining Star</td>
+                    <td className="border border-yellow-200">Earth, Wind, and Fire</td>
+                    <td className="border border-yellow-200">1975</td>
                   </tr>
                 </tbody>
               </table>
+              </div>
             </div>
             <div className="flex items-center justify-center mt-4 ">
               <a href="#" className="text-xl font-medium text-indigo-500">
