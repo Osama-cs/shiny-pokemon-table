a224059552aff8107060a6611eb1f4f76a8bceb2
Osama <oilyascs@gmail.com>
2023-08-08 23:54:07 +0100

    table displays pokemon name and link to shiny

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,21 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getPokemons } from "~/models/pokemon.server";
+
+type LoaderData = {
+  data: Awaited<ReturnType<typeof getPokemons>>;
+};
+
+export const loader = async () => {
+  return json<LoaderData>({
+    data: await getPokemons(),
+  });
+};
+
 export default function Index() {
+  const { data } = useLoaderData() as LoaderData;
   return (
-    <body className="h-screen bg-sky-500">
+    <body className="h-full bg-sky-500">
       <div>
         <h1 className="text-3xl font-bold underline text-center text-white">
           ShinyDex
@@ -8,43 +23,35 @@ export default function Index() {
         <div className="flex justify-center items-center">
           <div className="max-w-md py-4 px-8 shadow-lg rounded-lg my-20">
             <div>
-              <h2 className=" flex items-center justify-center text-gray-800 text-3xl font-semibold text-center ">
-                Design Tools
-              </h2>
-              <br/>
               <div className="">
-              <table className="table-fixed border-separate border border-yellow-200">
-                <thead>
-                  <tr>
-                    <th className="border border-yellow-200 w-1/3">Song</th>
-                    <th className="border border-yellow-200 w-1/3">Artist</th>
-                    <th className="border border-yellow-200 w-1/3">Year</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="border border-yellow-200">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    <td className="border border-yellow-200">Malcolm Lockyer</td>
-                    <td className="border border-yellow-200">1961</td>
-                  </tr>
-                  <tr>
-                    <td className="border border-yellow-200">Witchy Woman</td>
-                    <td className="border border-yellow-200">The Eagles</td>
-                    <td className="border border-yellow-200">1972</td>
-                  </tr>
-                  <tr>
-                    <td className="border border-yellow-200">Shining Star</td>
-                    <td className="border border-yellow-200">Earth, Wind, and Fire</td>
-                    <td className="border border-yellow-200">1975</td>
-                  </tr>
-                </tbody>
-              </table>
+                <table className="table-fixed border-separate border border-yellow-200">
+                  <thead>
+                    <tr>
+                      <th className="border border-yellow-200 w-1/3">
+                        Pokemon Name
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="border border-yellow-200">
+                        <ul className="mx-auto text-center">
+                          {data.map((pokemon: any) => (
+                            <li key={pokemon.name}>
+                              <Link
+                                to={pokemon.name}
+                                className="text-blue-600 underline"
+                              >
+                                {pokemon.name}
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
-            </div>
-            <div className="flex items-center justify-center mt-4 ">
-              <a href="#" className="text-xl font-medium text-indigo-500">
-                John Doe
-              </a>
             </div>
           </div>
         </div>
@@ -52,4 +59,3 @@ export default function Index() {
     </body>
   );
 }
-
