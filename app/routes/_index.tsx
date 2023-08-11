2dd1fa64c37d8cae27972fc5b935a61d041f6625
Osama <oilyascs@gmail.com>
2023-08-11 22:50:29 +0100

    finished the landing page.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,32 +15,45 @@ export const loader = async () => {
 export default function Index() {
   const { data } = useLoaderData() as LoaderData;
   return (
-    <body className="h-full bg-sky-500">
+    <body className="h-full bg-zinc-100">
       <div>
-        <h1 className="text-3xl font-bold underline text-center text-white">
-          ShinyDex
-        </h1>
+        <br />
+        <header>
+          <h1 className="text-3xl font-bold underline text-center text-sky-400 font-serif">
+            <span className="text-3xl font-bold underline text-center text-sky-400">
+              Shiny
+            </span>
+            <span className="text-3xl font-bold underline text-center text-black">
+              Dex
+            </span>
+          </h1>
+        </header>
+        <h2 className="text-center">
+          Welcome to ShinyDex! A place that you can view all of the shiny
+          pokemon from Generation 1.
+        </h2>
+        <h2 className="text-center">
+          Click on the name of the shiny pokemon you want to see and you will be greeted with that shiny!
+        </h2>
         <div className="flex justify-center items-center">
-          <div className="max-w-md py-4 px-8 shadow-lg rounded-lg my-20">
+          <div className="max-w-md py-4 px-8 shadow-lg rounded-lg my-5 bg-white">
             <div>
               <div className="">
-                <table className="table-fixed border-separate border border-yellow-200">
+                <table className="table-fixed shadow-lg bg-white border-separate">
                   <thead>
                     <tr>
-                      <th className="border border-yellow-200 w-1/3">
-                        Pokemon Name
-                      </th>
+                      <th className=" bg-blue-100 border">Pokemon Name</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td className="border border-yellow-200">
-                        <ul className="mx-auto text-center">
+                      <td className="border border-black">
+                        <ul className="mx-auto text-center divide-y divide-black">
                           {data.map((pokemon: any) => (
                             <li key={pokemon.name}>
                               <Link
                                 to={pokemon.name}
-                                className="text-blue-600 underline"
+                                className="underline hover:text-sky-400"
                               >
                                 {pokemon.name}
                               </Link>
